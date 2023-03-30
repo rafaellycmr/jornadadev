819603f9436c09dc69f84178b03f86ba99cdb326
Rafaelly <rafaellycmr@gmail.com>
2023-03-30 16:04:26 -0300

    atualizando o projeto - rodar video

---
+++ src/pages/Video.js
@@ -0,0 +1,34 @@
+import React, {useRef, useState} from 'react'
+import "./video.css"
+
+function Video() {
+   
+    const videoRef = useRef(null)
+    const [play,setPlay] = useState(false)
+
+    function handdleStart(){
+        if(play){
+            videoRef.current.pause()
+            setPlay(false)
+        }else{
+            videoRef.current.play()
+            setPlay(true)
+        }
+    }
+
+  return (
+    <div className='video'>
+        <video 
+            className='video__player'
+            ref={videoRef}
+            onClick={handdleStart}
+            loop
+            src="https://firebasestorage.googleapis.com/v0/b/jornada-b5e9d.appspot.com/o/brecker2.mp4?alt=media&token=9aa979e9-0907-4093-abd5-a71efe4fc946"
+        >
+
+        </video>
+    </div>
+  )
+}
+
+export default Video
